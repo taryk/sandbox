ebd7c864c395a8fe1a23ae21e45dd90dedf6ff04
Taras Yagniuk <mrtaryk@gmail.com>
2012-12-22 04:44:00 +0200

    ruby implementation of `luhn-checksum`

---
+++ ruby/luhn-checksum/luhn-checksum.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def split_d(strn)
+  strn.split("").map{|c| c.to_i}
+end
+
+def get_luhn_checksum(digits)
+  digits.each_with_index.map{|d,i| [i,d]}.group_by{|x| x[0] % 2 != 0}.map{|odd,set|
+    set = set.map{|x| x[1]}
+    (odd ? set : set.map{|x| x*=2; x > 9 ? x - 9 : x}).inject{|sum,x| sum+x}
+  }.inject{|sum,x| sum+x}
+end
+
+def is_valid(checksum)
+  checksum % 10 == 0
+end
+
+print "Card number: "
+card_n = gets
+
+puts "Card number is " + (is_valid(get_luhn_checksum(split_d(card_n))) ? "OK" : "Wrong")
+
+# -*- ruby -*-
