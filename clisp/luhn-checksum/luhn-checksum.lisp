08dde59f95cc800cd1e9da95fc23674f09e8080f
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-13 03:26:44 +0200

    clisp: [luhn-checksum]: add support of odd length of an input number

--- clisp/luhn-checksum/luhn-checksum.lisp
+++ clisp/luhn-checksum/luhn-checksum.lisp
@@ -1,15 +1,13 @@
 (defun get-luhn-checksum (number-str)
-  (let ((digits (coerce number-str 'list))
-        (n -1))
+  (let ((n (if (= (rem (length number-str) 2) 0) -1 0)))
     (apply #'+
            (map 'list #'(lambda(char)
-                          (incf n)
                           (let ((d (parse-integer (string char))))
-                            (if (= (rem n 2) 0)
+                            (if (= (rem (incf n) 2) 0)
                                 (let ((d2 (* d 2)))
                                   (if (> d2 9) (- d2 9) d2))
                                 d)))
-                digits))))
+                (coerce number-str 'list)))))
 
 (defun is-valid (checksum)
   (= (rem checksum 10) 0))
