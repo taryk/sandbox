c59a37eaf131747f830fe6014d6f8a26d7357664
Taras Iagniuk <mrtaryk@gmail.com>
2014-05-11 01:37:34 +0300

    Import java classes at the beginning, so they can be used without a namespace prefix.

--- clojure/timezones/src/timezones/core.clj
+++ clojure/timezones/src/timezones/core.clj
@@ -1,25 +1,26 @@
 (ns timezones.core
   (:gen-class)
+  (:import [java.util TimeZone Calendar]
+           [java.util.concurrent TimeUnit])
   (:use clojure.pprint))
 
 (defn timezones
   []
-  (for [timezone-id (java.util.TimeZone/getAvailableIDs)]
-    (let [timezone (java.util.TimeZone/getTimeZone timezone-id)
+  (for [timezone-id (TimeZone/getAvailableIDs)]
+    (let [timezone (TimeZone/getTimeZone timezone-id)
           raw-offset (.getRawOffset timezone)
-          hours-offset (.toHours (java.util.concurrent.TimeUnit/MILLISECONDS) raw-offset)
-          minutes-offset (.toMinutes (java.util.concurrent.TimeUnit/MILLISECONDS) raw-offset)]
+          hours-offset (.toHours (TimeUnit/MILLISECONDS) raw-offset)
+          minutes-offset (.toMinutes (TimeUnit/MILLISECONDS) raw-offset)]
       {:id (.getID timezone)
        :offset {:hours hours-offset
                 :minutes (- minutes-offset
-                            (.toMinutes (java.util.concurrent.TimeUnit/HOURS) hours-offset))}})))
+                            (.toMinutes (TimeUnit/HOURS) hours-offset))}})))
 
 (defn get-timezones
   [hour]
   (filter #(= hour
-              (.get
-               (java.util.Calendar/getInstance (java.util.TimeZone/getTimeZone (:id %)))
-               java.util.Calendar/HOUR_OF_DAY))
+              (.get (Calendar/getInstance (TimeZone/getTimeZone (:id %)))
+                    Calendar/HOUR_OF_DAY))
           (timezones)))
 
 (defn -main
